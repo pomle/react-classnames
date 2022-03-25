c5014c635a9012144b17e81a7875e8af9cd19337
Pontus Alexander <pontus.alexander@gmail.com>
2022-03-25 08:54:03 +0100

    Port over classNames function

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,48 @@
-import { noop } from "../index";
+import { classNames } from "../index";
 
-describe("index", () => {
-  it("runs a test", () => {
-    expect(noop()).toBe(undefined);
+describe("classNames", () => {
+  it("creates class entry from object keys and booleans", () => {
+    const text = classNames({
+      ref1: true,
+      ref2: false,
+      ref3: true,
+      ref4: true,
+    });
+
+    expect(text).toEqual("ref1 ref3 ref4");
+  });
+
+  it("creates class entry from string", () => {
+    const text = classNames("class1", "class2");
+
+    expect(text).toEqual("class1 class2");
+  });
+
+  it("unrolls arrays", () => {
+    const text = classNames(["class1", "class2"]);
+
+    expect(text).toEqual("class1 class2");
+  });
+
+  it("handles args in any order and combination", () => {
+    const text = classNames(
+      ["list1", "list2"],
+      "string2",
+      {
+        key1: true,
+        key2: true,
+        key3: false,
+        key4: false,
+        key5: true,
+      },
+      "string1",
+      { key6: true, key7: true },
+      "string2",
+      ["list3"],
+    );
+
+    expect(text).toEqual(
+      "list1 list2 string2 key1 key2 key5 string1 key6 key7 string2 list3",
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export function noop() {}
+type ClassEntry = Record<string, boolean> | string[] | string;
+
+export function classNames(...entries: ClassEntry[]) {
+  const names: string[] = [];
+  for (const entry of entries) {
+    if (typeof entry === "string") {
+      names.push(entry);
+    } else if (Array.isArray(entry)) {
+      names.push(...entry);
+    } else {
+      for (const [key, value] of Object.entries(entry)) {
+        if (value) {
+          names.push(key);
+        }
+      }
+    }
+  }
+  return names.join(" ");
+}
